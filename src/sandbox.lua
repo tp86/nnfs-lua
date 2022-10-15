d51adb2a95c776993256c5997de6fba51b16e445
tp86 <tom0palka+tp86@gmail.com>
2022-10-15 23:23:48 +0200

    WIP array/matrix library

--- src/sandbox.lua
+++ src/sandbox.lua
@@ -4,5 +4,9 @@ local v2 = { 2, 3, 4 }
 local dot = require('helpers').vdot
 local add = require('helpers').add
 
-print(dot(v1, v2))
-print(table.unpack(add(v1, v2)))
+--print(dot(v1, v2))
+--print(table.unpack(add(v1, v2)))
+
+local arr = require('arrays')
+
+arr.dot(arr.Mat{1, 2, 3}, arr.Mat{{4}, {5}, {6}})
